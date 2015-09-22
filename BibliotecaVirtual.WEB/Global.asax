2ad74509a23c7d900fd52a561ea9d7c195044f41
LeyZem <tdb_mc@hotmail.com>
2015-09-21 21:28:58 -0300

    Criação do Repositório

---
+++ BibliotecaVirtual.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BibliotecaVirtual.WEB.MvcApplication" Language="C#" %>
---
+++ BibliotecaVirtual.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BibliotecaVirtual.WEB.Startup))]
+namespace BibliotecaVirtual.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
